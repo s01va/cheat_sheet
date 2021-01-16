550e0d39c3ac1eb301269cdf715ee3791f6bf2a4
s01va <jinn0525@gmail.com>
2021-01-17 00:00:25 +0900

    prime number

--- algorithm/prime_number.py
+++ algorithm/prime_number.py
@@ -2,11 +2,15 @@
 # 소수 판별 알고리즘
 # 에라토스테네스의 체를 인용하여, 입력된 수의 루트값까지 판별하는 함수.
 
-def sieve(int num):
+def sieve(num):
 	if num != 1:
 		for i in range(2, num):
 			if num % i == 0:
-				False
+				return False
 	else:
-		False
+		return False
 	return True
+
+for i in range(1, 100):
+	if sieve(i) == True:
+		print(i)
